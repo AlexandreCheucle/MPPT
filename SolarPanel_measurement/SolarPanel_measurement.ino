31dbfeebe6c6e1b0691dbc399b6ebe8ae4d2445a
Antonin <antoninrenard12@gmail.com>
2019-01-14 10:54:59 +0100

    New sensor pin

--- SolarPanel_measurement/SolarPanel_measurement.ino
+++ SolarPanel_measurement/SolarPanel_measurement.ino
@@ -1,5 +1,5 @@
-const int sensorPin = A0;
-
+const int sensorPin1 = A0;
+const int sensorPin2 = A1;
 void setup()
 {
 	Serial.begin(9600);	
@@ -7,9 +7,8 @@ void setup()
 
 void loop()
 {
-	int sensorVal = analogRead(sensorPin);
+	int sensorVal = analogRead(sensorPin1);
 	float voltage = (sensorVal / 1024.0) * 5.0;
 	Serial.print(voltage);
 	Serial.print("\n");
 }
-
