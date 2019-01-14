7fbc895835f9f5a43a4a007901c97db0f1abc03c
Unknown <cheucle.alexandre@gmail.com>
2019-01-14 15:51:26 +0100

    Add control pin
    Change on circuit allow us to control a transistor for the Buck converter

--- SolarPanel_measurement/SolarPanel_measurement.ino
+++ SolarPanel_measurement/SolarPanel_measurement.ino
@@ -1,21 +1,43 @@
-const int sensorPin1 = A0;
-const int sensorPin2 = A1;
+const int sensorPin0 = A0;
+const int sensorPin1 = A1;
+const int Val_res = 20;
+const int controlpin = 2;
+
 void setup()
 {
 	Serial.begin(9600);	
+	pinMode(controlpin, OUTPUT);
 }
 
 void loop()
 {
-	int sensorVal1 = analogRead(sensorPin1);
-  int sensorVal2 = analogRead(sensorPin2);
-  float current = ((sensorVal2-sensorVal1)/1024.0)*5.0;
-	float voltage = (sensorVal1 / 1024.0) * 5.0;
-  Serial.print("V = ");
-	Serial.print(voltage);
+
+	digitalWrite(controlpin, HIGH);
+
+  int sensorVal0 = analogRead(sensorPin0);
+  int sensorVal1 = analogRead(sensorPin1);
+
+  float Val0 = (sensorVal0 / 1024.0) * 5.0;
+  float Val1 = ((sensorVal1)/1024.0)*5.0;
+  
+  
+  Serial.print("V0 = ");
+	Serial.print(Val0);
+	Serial.print("V ");
+	Serial.print(sensorVal0);
 	Serial.print("\n");
-  Serial.print("I = ");
-  Serial.print(current);
+
+	Serial.print("V1 = ");
+	Serial.print(Val1);
+	Serial.print("V ");
+	Serial.print(sensorVal1);
+	Serial.print("\n");
+  
+
+  Serial.print("I0 = ");
+  Serial.print((Val1-Val0/Val_res) * 1000);
+  Serial.print("mA");
   Serial.print("\n");
-  delay(100);
+  
+  delay(1000);
 }
