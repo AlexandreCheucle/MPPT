7029da955ddb6417d3a112e667d7c6603110aa98
Antonin <antoninrenard12@gmail.com>
2019-01-14 11:07:01 +0100

    Current

--- SolarPanel_measurement/SolarPanel_measurement.ino
+++ SolarPanel_measurement/SolarPanel_measurement.ino
@@ -7,8 +7,15 @@ void setup()
 
 void loop()
 {
-	int sensorVal = analogRead(sensorPin1);
-	float voltage = (sensorVal / 1024.0) * 5.0;
+	int sensorVal1 = analogRead(sensorPin1);
+  int sensorVal2 = analogRead(sensorPin2);
+  float current = ((sensorVal2-sensorVal1)/1024.0)*5.0;
+	float voltage = (sensorVal1 / 1024.0) * 5.0;
+  Serial.print("V = ");
 	Serial.print(voltage);
 	Serial.print("\n");
+  Serial.print("I = ");
+  Serial.print(current);
+  Serial.print("\n");
+  delay(100);
 }
