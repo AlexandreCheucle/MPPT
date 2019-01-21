3d029041aa06a381d79c987a50f70359e1ac2eb1
Unknown <cheucle.alexandre@gmail.com>
2019-01-21 10:02:55 +0100

    Motor Update
    Add function to control a motor via an H bridge

--- SolarPanel_measurement/SolarPanel_measurement.ino
+++ SolarPanel_measurement/SolarPanel_measurement.ino
@@ -2,12 +2,20 @@ const int sensorPin0 = A0;
 const int sensorPin1 = A1;
 const int Val_res = 20;
 const int controlpin = 3;
-const int alpha = 50;
+const int alpha = 255;
+
+int pin1Moteur1=6; //pin de commande moteur 1
+int pin2Moteur1=7; // pin de commande moteur 1
+int pinPMoteur1=5;// pin PWM moteur 1
 
 void setup()
 {
 	Serial.begin(9600);	
 	pinMode(controlpin, OUTPUT);
+
+	pinMode(pin1Moteur1,OUTPUT);
+  	pinMode(pin2Moteur1,OUTPUT);
+	pinMode(pinPMoteur1,OUTPUT);
 }
 
 void loop()
@@ -33,12 +41,59 @@ void loop()
 	Serial.print("V ");
 	Serial.print(sensorVal1);
 	Serial.print("\n");
+
+	actionMoteur(1,100);
   
 
-  Serial.print("I0 = ");
+  /*Serial.print("I0 = ");
   Serial.print((Val1-Val0/Val_res) * 1000);
   Serial.print("mA");
-  Serial.print("\n");
+  Serial.print("\n");*/
   
   delay(1000);
+}
+
+
+void actionMoteur(int sens,int pourcentage){ // inspiré du code OC : https://openclassrooms.com/fr/courses/2778161-programmez-vos-premiers-montages-avec-arduino/3285355-le-moteur-a-courant-continu-partie-2-le-pont-en-h-et-les-circuits-integres
+
+  int pin1,etat1,pin2,etat2,pinP,puissance; //variable de la fonction
+
+    pin1=pin1Moteur1;
+    pin2=pin2Moteur1;
+    pinP=pinPMoteur1;
+
+  //test sens du moteur 1,-1 (sens contrainre) ou tout autre valeur (stoppe le moteur)
+
+  if (sens==1){
+    etat1=1;
+    etat2=0;
+  }
+  else if (sens==-1){
+    etat1=0;
+    etat2=1;
+  }
+  else {
+    etat1=0;
+    etat2=0;
+  }
+
+  puissance=map(pourcentage,0,100,0,255);
+
+  analogWrite(pinP,puissance);
+  digitalWrite(pin1,etat1);
+  digitalWrite(pin2,etat2);
+
+  //affichage sur le moniteur série (facultatif)
+  
+
+  if (sens==-1 || sens==1){
+    Serial.print(" sens : ");
+    Serial.print(sens);
+  }
+
+  else {
+    Serial.print(" ! stop ! ");
+  }
+  Serial.print(" puissance : ");
+  Serial.println(pourcentage);
 }
